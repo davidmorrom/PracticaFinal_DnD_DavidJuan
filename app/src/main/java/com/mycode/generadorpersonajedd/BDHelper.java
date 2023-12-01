01786cf68ebf2e8e8d9d83992ce27b74a4aa47d4
David <david.morrom.contact@gmail.com>
2023-12-01 04:49:05 +0100

    BDHelper

---
+++ app/src/main/java/com/mycode/generadorpersonajedd/BDHelper.java
@@ -0,0 +1,37 @@
+package com.mycode.generadorpersonajedd;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class BDHelper extends SQLiteOpenHelper {
+    public BDHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_TABLE_PERSONAJES = "CREATE TABLE Personajes (" +
+                "NombreJugador TEXT," +
+                "NombrePersonaje TEXT," +
+                "Clase TEXT," +
+                "Fuerza INTEGER," +
+                "Destreza INTEGER," +
+                "Constitucion INTEGER," +
+                "Inteligencia INTEGER," +
+                "Sabiduria INTEGER," +
+                "Carisma INTEGER," +
+                "Habilidades TEXT," +
+                "PRIMARY KEY (NombreJugador, NombrePersonaje))";
+        db.execSQL(CREATE_TABLE_PERSONAJES);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS Personajes");
+        onCreate(db);
+    }
+
+}
