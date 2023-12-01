28d7ff6e910efa478c40aab3bc278d8eaacda735
David <david.morrom.contact@gmail.com>
2023-12-01 23:03:19 +0100

    Estadisticas Layout

--- app/src/main/java/com/mycode/generadorpersonajedd/Estadisticas.java
+++ app/src/main/java/com/mycode/generadorpersonajedd/Estadisticas.java
@@ -1,4 +1,12 @@
 package com.mycode.generadorpersonajedd;
 
-public class Estadisticas {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Estadisticas extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estadisticas);
+    }
 }
