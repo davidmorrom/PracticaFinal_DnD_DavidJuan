6891653b3b87f5dcf4af9a7bff04669e8104865d
juan <juanantoniocastromontero@gmail.com>
2023-12-02 18:26:12 +0100

    Insert de datos en la base de datos

--- app/src/main/java/com/mycode/generadorpersonajedd/MainActivity.java
+++ app/src/main/java/com/mycode/generadorpersonajedd/MainActivity.java
@@ -2,7 +2,10 @@ package com.mycode.generadorpersonajedd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,12 +24,14 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private EditText editTextNombrePersonaje;
     private Spinner spinnerClase;
-    private String [] clases_dnd;;
-    private int [] imagenes_dnd;
+    private String[] clases_dnd;
+    ;
+    private int[] imagenes_dnd;
     private int[] estadisticas = new int[6];
-    ArrayList<String> skills = new ArrayList<>();
+    private ArrayList<String> skills = new ArrayList<>();
 
-    Button botonGuardar,botonEstadisticas,botonHabilidades;
+    private Button botonGuardar, botonEstadisticas, botonHabilidades;
+    private BDHelper BDHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +43,9 @@ public class MainActivity extends AppCompatActivity {
 
         botonGuardar.setActivated(false);
         clases_dnd = getResources().getStringArray(R.array.clases_dnd);
-        imagenes_dnd = new int[]{R.drawable.barbaro, R.drawable.bardo, R.drawable.brujo, R.drawable.clerigo, R.drawable.druida, R.drawable.explorador, R.drawable.guerrero, R.drawable.hechicero, R.drawable.mago, R.drawable.monje, R.drawable.paladin, R.drawable.picaro};
+        imagenes_dnd = new int[]{
+                R.drawable.barbaro, R.drawable.bardo, R.drawable.brujo, R.drawable.clerigo, R.drawable.druida, R.drawable.explorador, R.drawable.guerrero, R.drawable.hechicero, R.drawable.mago, R.drawable.monje, R.drawable.paladin, R.drawable.picaro
+        };
 
         editTextNombrePersonaje = findViewById(R.id.editTextNombrePersonaje);
         spinnerClase = findViewById(R.id.spinnerClase);
@@ -71,28 +78,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void crearPersonaje() {
-        String nombrePersonaje = editTextNombrePersonaje.getText().toString();
-        String clasePersonaje = spinnerClase.getSelectedItem().toString();
+        String nombrePersonaje = editTextNombrePersonaje.getText().toString().trim();
+        String clasePersonaje = spinnerClase.getSelectedItem().toString().trim();
         if (!nombrePersonaje.isEmpty()) {
+            ContentValues values = new ContentValues();
+            values.put("NombreJugador", nombrePersonaje);
+            values.put("NombrePersonaje", nombrePersonaje);
+            values.put("Clase", clasePersonaje);
+            values.put("Fuerza", estadisticas[0]);
+            values.put("Destreza", estadisticas[1]);
+            values.put("Constitucion", estadisticas[2]);
+            values.put("Inteligencia", estadisticas[3]);
+            values.put("Sabiduria", estadisticas[4]);
+            values.put("Carisma", estadisticas[5]);
+            values.put("Habilidades", skills.toString());
+            SQLiteDatabase database = BDHelper.getBDWriteable();
+            long newRowId = database.insert("Personajes", null, values);
             Toast.makeText(this, "Se ha introducido con exito", Toast.LENGTH_SHORT).show();
+            editTextNombrePersonaje.setText("");
+            spinnerClase.setSelection(0);
+            skills.clear();
+            estadisticas = new int[6];
+            botonGuardar.setActivated(false);
         } else {
             Toast.makeText(this, "Por favor, introduce un nombre de personaje", Toast.LENGTH_SHORT).show();
         }
     }
 
-    public void elegirHabilidades(){
+    public void elegirHabilidades() {
         Intent intent = getIntent();
         skills = (ArrayList<String>) intent.getExtras().get("Skills-selecionadas");
     }
-    public void ponerEstadisticas(){
+
+    public void ponerEstadisticas() {
         Intent intent = getIntent();
         estadisticas = (int[]) intent.getExtras().get("estadisticas");
     }
-    public void actualizarBoton(){
+
+    public void actualizarBoton() {
         if (!skills.isEmpty() && estadisticas[0] != 0 && estadisticas[1] != 0 && estadisticas[2] != 0 && estadisticas[3] != 0 && estadisticas[4] != 0 && estadisticas[5] != 0) {
             botonGuardar.setActivated(true);
         }
     }
+
     class ClasesAdapter extends BaseAdapter {
         @Override
         public int getCount() {
